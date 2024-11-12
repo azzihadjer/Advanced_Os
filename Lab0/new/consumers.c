53b893c06c336ca4de92e20b4e58ccaa4941ae89
azzihadjer <101755091+azzihadjer@users.noreply.github.com>
2024-11-12 23:43:37 +0100

    Update consumers.c

--- Lab0/new/consumers.c
+++ Lab0/new/consumers.c
@@ -1,52 +1,27 @@
-#include "shared_memory.h"
+#include "shared.h"
 #include <pthread.h>
 
 void *consumer(void *arg) {
-    struct shared_memory *shm = (struct shared_memory *)arg;
+    struct consumer_args *args = (struct consumer_args *)arg;
+    struct shared_memory *shm = args->shm;
+    int consumer_id = args->consumer_id;
+    free(args); 
+
     int item;
-    int consumer_id = *((int *)arg);  
 
     while (1) {
         sem_wait(&shm->full);  
         pthread_mutex_lock(&shm->mutex);  
 
-        
         item = shm->buffer[shm->out];
         printf("[ Consumer %d ]: consumed item %d from index %d\n", consumer_id, item, shm->out);
         shm->out = (shm->out + 1) % BUFFER_SIZE; 
 
         pthread_mutex_unlock(&shm->mutex);  
-        sem_post(&shm->empty);  Signal that the buffer has space
+        sem_post(&shm->empty);  
 
         sleep(rand() % 3);  
     }
 
     return NULL;
-}
-
-int main() {
-    pthread_t cons_threads[NUM_CONSUMERS];
-    struct shared_memory shm;
-    shm.in = 0;
-    shm.out = 0;
-
-    
-    sem_init(&shm.empty, 0, BUFFER_SIZE);
-    sem_init(&shm.full, 0, 0);
-    pthread_mutex_init(&shm.mutex, NULL);
-
-    
-    int consumer_ids[NUM_CONSUMERS] = {1, 2,3};
-
-   
-    create_consumer_threads(cons_threads, &shm, consumer_ids);
-
-    join_consumer_threads(cons_threads);
-
-  
-    sem_destroy(&shm.empty);
-    sem_destroy(&shm.full);
-    pthread_mutex_destroy(&shm.mutex);
-
-    return 0;
 }
