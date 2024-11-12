bf73ee4c65d8f7aa519ecfc9be63171fc1a69c60
azzihadjer <101755091+azzihadjer@users.noreply.github.com>
2024-11-12 23:42:04 +0100

    Update shared_memory.h

--- Lab0/new/shared_memory.h
+++ Lab0/new/shared_memory.h
@@ -11,17 +11,25 @@
 #define NUM_PRODUCERS 3
 #define NUM_CONSUMERS 3
 
-
 struct shared_memory {
     int buffer[BUFFER_SIZE];
     int in;
     int out;
-
     sem_t empty;  
     sem_t full;  
     pthread_mutex_t mutex;  
 };
 
+
+struct producer_args {
+    struct shared_memory *shm;
+    int producer_id;
+};
+
+struct consumer_args {
+    struct shared_memory *shm;
+    int consumer_id;
+};
 
 void *producer(void *arg);
 void *consumer(void *arg);
@@ -30,4 +38,4 @@ void create_consumer_threads(pthread_t *cons_threads, struct shared_memory *shm,
 void join_producer_threads(pthread_t *prod_threads);
 void join_consumer_threads(pthread_t *cons_threads);
 
-#endif
+#endif 
